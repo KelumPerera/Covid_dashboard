f81932a16fc7219e42c24aeb3ab0e3813f12edea
Kelum Perera <kelum0823@hotmail.com>
2021-05-07 15:57:17 +0530

    initial commit

--- covid_final/settings.py
+++ covid_final/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['covidklm.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['covidklm.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
